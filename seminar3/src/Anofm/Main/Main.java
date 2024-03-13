c19c036aa5c246e228f49e8b09be18fbc29cf35e
mirunamia <munteanumiruna20@stud.ase.ro>
2024-03-13 14:06:22 +0200

    Adaugat seminar 3

---
+++ seminar3/src/Anofm/Main/Main.java
@@ -0,0 +1,13 @@
+package Anofm.Main;
+
+import Anofm.ManagerRegistry;
+
+public class Main {
+    public static void main(String[] args) {
+        ManagerRegistry registru = ManagerRegistry.getInstance();
+        registru.inregistreazaManager("CTS SRL", "Popescu", 10000f);
+        registru.inregistreazaManager("CTS SRL", "Ionescu", 20000f);
+        System.out.println(registru.getManager("CTS SRL"));
+    }
+}
+
